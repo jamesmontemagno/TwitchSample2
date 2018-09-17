b45e87558956d42838fd994f3572315ab552e2d0
James Montemagno <james.montemagno@gmail.com>
2018-09-16 19:40:36 -0700

    Add project files.

---
+++ TwitchSample2/TwitchSample2/CountViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+using MvvmHelpers;
+
+namespace TwitchSample2
+{
+    public class CountViewModel : BaseViewModel
+    {
+        public CountViewModel()
+        {
+            IncreaseCountCommand = new Command(IncreaseCount);
+
+            if (Xamarin.Forms.DesignMode.IsDesignModeEnabled)
+            {
+                Count = 10;
+                CountInfo = $"You clicked {Count} times!";
+            }
+        }
+        int count = 0;
+        public int Count
+        {
+            get => count;
+            set => SetProperty(ref count, value);
+        }
+
+        string countInfo = string.Empty;
+        public string CountInfo
+        {
+            get => countInfo;
+            set => SetProperty(ref countInfo, value);
+        }
+
+        public Command IncreaseCountCommand { get; }
+
+        void IncreaseCount()
+        {
+            Count++;
+            CountInfo = $"You clicked {Count} times!";
+        }
+
+    }
+}
---
+++ TwitchSample2/TwitchSample2/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TwitchSample2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            if(Xamarin.Forms.DesignMode.IsDesignModeEnabled)
+            {
+                BindingContext = new CountViewModel();
+            }
+        }
+    }
+}
